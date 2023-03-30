f8a387620f70fa48c7acc8c74a5a136d258b3eaf
warnib <thapaliya39@gmail.com>
2023-03-29 20:21:10 -0500

    Code changed

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var balance = 1000;
+var balance = 0;
+var balanceElement = document.getElementById("balance");
 
-function processTransaction() {
+function earn() {
 	var amount = parseFloat(document.getElementById("amount").value);
-	var transactionType = document.getElementById("transactionType").value;
-	
-	if (transactionType === "deposit") {
-		deposit(amount);
-	} else if (transactionType === "withdraw") {
-		withdraw(amount);
+	if (amount > 0) {
+		balance += amount;
+		updateBalance();
 	}
 }
 
-function deposit(amount) {
-	balance += amount;
-	drawTransaction(amount, "green", "Deposited");
-}
-
-function withdraw(amount) {
-	if (balance >= amount) {
+function spend() {
+	var amount = parseFloat(document.getElementById("amount").value);
+	if (amount > 0 && amount <= balance) {
 		balance -= amount;
-		drawTransaction(amount, "red", "Withdrawn");
-	} else {
-		drawTransaction(amount, "orange", "Insufficient Funds");
+		updateBalance();
 	}
 }
 
-function drawTransaction(amount, color, message) {
-	ctx.fillStyle = color;
-	ctx.fillRect(10, 10, 200, 50);
-	ctx.fillStyle = "white";
-	ctx.font = "20px Arial";
-	ctx.fillText(message + " " + amount.toFixed(2), 20, 40);
-	ctx.fillText("New Balance: " + balance.toFixed(2), 20, 70);
+function updateBalance() {
+	balanceElement.innerText = balance.toFixed(2);
 }
